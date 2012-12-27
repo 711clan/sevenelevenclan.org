f431704dcf7edf7d6abf45b894df3ad5d9182a26
Steven Robbins <stevether@gmail.com>
2012-12-26 19:07:15 -0800

    fix syntax error

--- mapratings/vsh.php
+++ mapratings/vsh.php
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-include 'connection.php'
+include 'connection.php';
 define('MR_DBNAME', 'maprate3');
 define('MR_TABLENAME', 'map_ratings');
 define('MR_THRESHOLD', 0);
